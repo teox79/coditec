b2d170553e596f39e7dcaa7bdd2f78cc22cf8af4
teox79 <teox1979@gmail.com>
2024-09-16 18:08:27 +0200

    add husky

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,7 +2,6 @@ import { AboutState } from "../context/AboutTypes";
 
 
 export const aboutData: AboutState = {
-    id: "about",
     title: "Chi Siamo",
     description: "Coditec è un comitato che si compone di un team di professionisti e volontari che collaborano per diffondere a tutti la conoscenza tecnologica e informatica sia in età scolare che adulta, come le nozioni base per imparare a programmare. Ogni collaboratore dedica con passione il suo tempo per divulgare queste conoscenze. I nostri corsi si tengono in Brianza a Lentate Sul Seveso ma siamo ben lieti di offrire i nostri servizi anche per i cittadini delle province limitrofe e non solo. Negli anni abbiamo collaborato con i comuni di Lentate, Barlassina, Birago, Copreno.",
     testimonial: {
