4ce186309b995210e8e3a892158c541aebdf2bd0
Matteo Balestrini <teox1979@gmail.com>
2024-09-17 21:27:04 +0200

    21 rimuovere filtro prezzo (#25)
    * feat: hide price filter
    
    feature 21
    
    * fix: fix apertura dettaglio corso

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { AppProvider } from './context/AppContext';
 import AppRoutes from './route/AppRoutes';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const App: React.FC = () => {
 
     return (
-        <HashRouter>
+        <BrowserRouter basename="/coditec">
             <AppProvider>
                 <Header />
                 <AppRoutes />
                 <Footer />
             </AppProvider>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
