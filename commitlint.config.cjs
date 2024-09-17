c265cb197fc2dc3c3f25918b8ca8b6c56c8f2904
teox79 <teox1979@gmail.com>
2024-09-17 14:43:14 +0200

    feat: fix lenght

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'footer-max-line-length': [0, 'never']
+        'footer-max-line-length': [0, 'never'],
+        'body-max-line-length': [0, 'always', 0]
     }
 };
