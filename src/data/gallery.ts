44f0555fb89198eff61b15cf9bea3a30548234f7
Matteo Balestrini <teox1979@gmail.com>
2024-09-19 01:21:34 +0200

    Feature/script (#49)
    * feat: add script rename image
    
    * fix: test
    
    * fix: add images
    
    * fix: remove images

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -5,6 +5,6 @@ export const galleryData: GalleryState = {
     id: getPersistentUUID("gallery"),
     title: "Galleria",
     description: "",
-    images: createImagesArray(24)
+    images: createImagesArray(30)
 }
 
