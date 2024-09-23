7e1e573f799a6524228e342bc14ade0fab7ea876
teox79 <teox1979@gmail.com>
2024-09-23 11:48:28 +0200

    build: remove double image

--- src/components/Common/About.tsx
+++ src/components/Common/About.tsx
@@ -17,13 +17,7 @@ const About: React.FC<AboutProps> = ({ title, subtitle, description, points }) =
                         data-aos="fade-up"
                         data-aos-delay="100"
                     >
-                        <div>IMG UNO {ui.globalUi.baseUrl} :
-                            <img src={`${ui.globalUi.baseUrl}assets/img/about/about.jpg`} className="img-fluid img-rounded" alt="About Us" />
-                        </div>
-                        <div>
-                            IMG DUE
-                            <img src={`assets/img/about/about.jpg`} className="img-fluid img-rounded" alt="About Us" />
-                        </div>
+                        <img src={`${ui.globalUi.baseUrl}assets/img/about/about.jpg`} className="img-fluid img-rounded" alt="About Us" />
                     </div>
 
                     {/* Contenuto About */}
