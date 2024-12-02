f380a4456114430c73405bf212c41cfd9962ab36
andreasironi <42619677+andreasironi@users.noreply.github.com>
2024-12-02 09:24:04 +0100

    Calendar fix (#101)
    * style: fix full calendar styles
    
    * style(fix calendar): fix calendar
    
    ---------
    
    Co-authored-by: Andrea Sironi <andrea.sironi@intre.it>

--- src/components/Common/FullCalendarCustom.tsx
+++ src/components/Common/FullCalendarCustom.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
-    EventClickArg,
-    EventContentArg,
+  EventClickArg,
+  EventContentArg,
 } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -15,43 +15,103 @@ import { Event as EventType } from '../../context/EventType';
 import { Offcanvas } from 'bootstrap';
 
 const transformEvents = (events: Array<any>) => {
-    return events.map(event => ({
-        id: event.id,
-        title: event.title,
-        start: event.startDate,
-        classNames: getEventClassName(event.eventType),
-        eventDate: event.date,
-        ...event
-    }));
+  return events.map(event => ({
+    id: event.id,
+    title: event.title,
+    start: event.startDate,
+    classNames: getEventClassName(event.eventType),
+    eventDate: event.date,
+    ...event
+  }));
 };
 
 // Funzione helper per mappare eventType a una classe CSS
 const getEventClassName = (eventType: string): string => {
-    return classNames("event", {
-        'coderdojo-event': eventType === 'coderdojo',
-        'coditec-event': eventType === 'coditec'
-    });
+  return classNames("event", {
+    'coderdojo-event': eventType === 'coderdojo',
+    'coditec-event': eventType === 'coditec'
+  });
 }
 
 
 const FullCalendarCustom: React.FC = () => {
-    const { state } = useAppContext();
-    const { event, ui } = state;
-    const [selectedEvent, setSelectedEvent] = React.useState<EventType>();
-    const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const { state } = useAppContext();
+  const { event, ui } = state;
+  const [selectedEvent, setSelectedEvent,] = React.useState<EventType>();
+  const [setSelectedEvents] = React.useState([]);
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
 
-    const handleEventClick = (clickInfo: EventClickArg) => {
-        const myOffcanvas = document.getElementById('EventOffcanvas');
-        if (myOffcanvas) {
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    const myOffcanvas = document.getElementById('EventOffcanvas');
+    if (myOffcanvas) {
 
-            const registration = clickInfo.event.extendedProps.registration;
-            setIsOpen(registration?.isOpen || false && registration?.url || false);
-            setSelectedEvent(clickInfo.event.extendedProps as EventType);
-            // @ts-ignore
-            const offcanvasInstance = new Offcanvas(myOffcanvas); // Crea un'istanza dell'Offcanvas
-            offcanvasInstance.show(); // Apri l'Offcanvas
-        }
+      const registration = clickInfo.event.extendedProps.registration;
+      setIsOpen(registration?.isOpen || false && registration?.url || false);
+      setSelectedEvent(clickInfo.event.extendedProps as EventType);
+      const offcanvasInstance = new Offcanvas(myOffcanvas); // Crea un'istanza dell'Offcanvas
+      offcanvasInstance.show(); // Apri l'Offcanvas
     }
+  }
+
+
+  // Uso della funzione con l'oggetto events
+  const events = transformEvents(event.events || []);
+  return (
+    <>
+      <section id="events_section" className="events section">
+        <div className="container">
+          <div className='row'>
+            <div className="container section-title" data-aos="fade-up">
+              <h2>CALENDARIO</h2>
+              <p>PROSSIMI EVENTI</p>
+            </div>
+            <div className='coditec-calendar'>
+              <div className='coditec-calendar-main'>
+                <FullCalendar
+                  plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                  headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    //right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    right: ''
+                  }}
+                  initialView='dayGridMonth'
+                  selectable={true}
+                  selectMirror={true}
+                  dayMaxEvents={true}
+                  weekends={true}
+                  initialEvents={events} 
+                  //select={this.handleDateSelect}
+                  eventContent={renderEventContent} // custom render function
+                  eventClick={handleEventClick}
+                  moreLinkText={(num) => `${num}`}
+                  //eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+                  /* you can update a remote database when these fire:
+                  eventAdd={function(){}}
+                  eventChange={function(){}}
+                  eventRemove={function(){}}
+                  */
+                  locale={'it'}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <OffCanvasCustom name="Evento" id="EventOffcanvas">
+        {selectedEvent && (
+          <div key={event.id} className="col-md-12 d-flex align-items-stretch">
+            <EventCard
+              event={selectedEvent}
+              isOpen={isOpen}
+              uiData={ui}
+              showImage={false}
+            />
+          </div>
+        )}
+      </OffCanvasCustom >
+    </>
+  )
 
     // Uso della funzione con l'oggetto events
     const events = transformEvents(event.events || []);
@@ -118,10 +178,10 @@ const FullCalendarCustom: React.FC = () => {
 export default FullCalendarCustom;
 
 function renderEventContent(eventContent: EventContentArg) {
-    return (
-        <>
-            <b>{eventContent.timeText}</b>
-            <i>{eventContent.event.title}</i>
-        </>
-    )
+  return (
+    <>
+      <b>{eventContent.timeText}</b>
+      <i>{eventContent.event.title}</i>
+    </>
+  )
 }
